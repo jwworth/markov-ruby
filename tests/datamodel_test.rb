54cc596dc58b13be10ecdac6bdaaf351b92e1f32
Jake Worth <jake@jakeworth.com>
2020-12-11 15:51:27 -0500

    Rename tests

--- tests/datamodel_test.rb
+++ tests/datamodel_test.rb
@@ -2,19 +2,19 @@ require 'minitest/autorun'
 require '../lib/data_model'
 
 class DataModelTest < Minitest::Test
-  def test_tokenizing
+  def test_tokenizes_basic_input
     result = DataModel.tokenize('One two?')
 
     assert_equal(['one', 'two', '?'], result)
   end
 
-  def test_tokenizing_multiple_delimiters
+  def test_tokenizes_two_delimiters
     result = DataModel.tokenize('One? two?')
 
     assert_equal(['one', '?', 'two', '?'], result)
   end
 
-  def test_tokenizing_commas
+  def test_tokenizes_commas
     result = DataModel.tokenize('One, two?')
 
     assert_equal(['one', ',', 'two', '?'], result)
@@ -26,14 +26,14 @@ class DataModelTest < Minitest::Test
     assert_equal({ 'one' => ['two'] }, result)
   end
 
-  def test_long_string
-    input = DataModel.prepare(File.read('tests/mock_data/gettysburg.txt')).to_s
-    result = File.read('tests/mock_data/gettysburg_parsed.rb')
+  def test_prepare_long_string
+    input = DataModel.prepare(File.read('tests/mock_data/gettysburg.txt'))
+    result = eval(File.read('tests/mock_data/gettysburg_parsed.rb'))
 
     assert_equal(input, result)
   end
 
-  def test_matches
+  def test_collects_multiple_matches
     result = DataModel.prepare('one two one three')
 
     assert_equal({ 'one' => %w[two three], 'two' => ['one'] }, result)
