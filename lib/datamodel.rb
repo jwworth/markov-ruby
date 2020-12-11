0c2ea13ebfebe73c3b11505fa567093d2f4288bd
Jake Worth <jake@jakeworth.com>
2020-12-11 15:04:57 -0500

    Small refactor

--- lib/datamodel.rb
+++ lib/datamodel.rb
@@ -2,18 +2,16 @@ class DataModel
   def self.prepare(input)
     tokens = input.split(/\s+/)
 
-    result = Hash.new([])
-    tokens.map.with_index do |token, index|
+    holder = Hash.new([])
+    tokens.each_with_object(holder).with_index do |(token, hash), index|
       next if index == tokens.length - 1
 
-      result[token] += [tokens[index + 1]]
+      hash[token] += [tokens[index + 1]]
     end
-
-    result
   end
 
-  def self.tokenize(input)
-    input.downcase.partition(/[\.\!\?]/).flat_map do |substring|
+  def self.tokenize(text)
+    text.downcase.partition(/[\.\!\?]/).flat_map do |substring|
       substring.split(/\s+/)
     end
   end
